a4cddae18c7d718b5eeceb817dc2afccadce0f8c
Joshua Phipps <jscottphipps@gmail.com>
2019-09-18 08:50:58 -0700

    Initialize repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CardContainer from "./components/Card Module/CardContainer";
+import Navbar from "./components/Navbar Module/Navbar";
+import "./App.scss";
 
 function App() {
+  const [type, setType] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar type={type} setType={setType} />
+      <CardContainer type={type} setType={setType} />
+    </main>
   );
 }
 
